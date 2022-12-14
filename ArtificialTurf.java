b0a063d64a47d59ba9c1dc8f11202e51d833c59b
KatyaMarinova <103966405+KatyaMarinova@users.noreply.github.com>
2022-12-14 23:20:23 +0200

    field

---
+++ ArtificialTurf.java
@@ -0,0 +1,64 @@
+package football.entities.field;
+
+import football.entities.player.Player;
+import football.entities.supplement.Supplement;
+
+import java.util.Collection;
+
+public class ArtificialTurf extends BaseField {
+    private static final int CAPACITY = 150;
+
+    public ArtificialTurf(String name) {
+        super(name, CAPACITY);
+    }
+
+    @Override
+    public void setName(String name) {
+        super.setName(name);
+    }
+
+    @Override
+    public int sumEnergy() {
+        return super.sumEnergy();
+    }
+
+    @Override
+    public void addPlayer(Player player) {
+        super.addPlayer(player);
+    }
+
+    @Override
+    public void removePlayer(Player player) {
+        super.removePlayer(player);
+    }
+
+    @Override
+    public void addSupplement(Supplement supplement) {
+        super.addSupplement(supplement);
+    }
+
+    @Override
+    public void drag() {
+        super.drag();
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo();
+    }
+
+    @Override
+    public Collection<Player> getPlayers() {
+        return super.getPlayers();
+    }
+
+    @Override
+    public Collection<Supplement> getSupplements() {
+        return super.getSupplements();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+}
